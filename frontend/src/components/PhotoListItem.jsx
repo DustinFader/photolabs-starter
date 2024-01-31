110acfc3acc9c613fa33e6054a04038610ddfd7c
DustinFader <dustinfader16@gmail.com>
2024-01-31 15:28:44 -0400

    add sass classNames

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,11 +4,13 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <div>
-      <img key={props.photo.id} src={props.photo.imageSource}></img>
-      <img src={props.photo.profile}></img>
-      <p>{props.photo.username}</p>
-      <p>{props.photo.location.city} {props.photo.location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" key={props.photo.id} src={props.photo.imageSource}></img>
+      <img className="photo-list__user-profile" src={props.photo.profile}></img>
+      <div className="photo-list__user-details">
+        <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
+        <p className="photo-list__user-info">{props.photo.username}</p>
+      </div>
     </div>
   );
 };
