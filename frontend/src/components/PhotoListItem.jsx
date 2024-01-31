b3b504311b9c9baeec85d80f658c6adef9484422
DustinFader <dustinfader16@gmail.com>
2024-01-31 15:43:21 -0400

    move placement of profile image

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,10 +6,10 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item">
       <img className="photo-list__image" key={props.photo.id} src={props.photo.imageSource}></img>
-      <img className="photo-list__user-profile" src={props.photo.profile}></img>
       <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.photo.profile}></img>
         <p className="photo-list__user-info">{props.photo.username}</p>
-        <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
+        <p className="photo-list__user-location">{props.photo.location.city}, {props.photo.location.country}</p>
       </div>
     </div>
   );
