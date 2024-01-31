49e9be58eadfe2aac68cfb370061a7c895692409
DustinFader <dustinfader16@gmail.com>
2024-01-31 15:39:27 -0400

    move info placement

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,8 +8,8 @@ const PhotoListItem = (props) => {
       <img className="photo-list__image" key={props.photo.id} src={props.photo.imageSource}></img>
       <img className="photo-list__user-profile" src={props.photo.profile}></img>
       <div className="photo-list__user-details">
-        <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
         <p className="photo-list__user-info">{props.photo.username}</p>
+        <p className="photo-list__user-location">{props.photo.location.city} {props.photo.location.country}</p>
       </div>
     </div>
   );
