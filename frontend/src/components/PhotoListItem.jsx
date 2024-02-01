643a36d429bdfabbf9304af92687cc9fedc9f92f
DustinFader <dustinfader16@gmail.com>
2024-02-01 11:05:37 -0400

    adjust profile info scss

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,8 +8,10 @@ const PhotoListItem = (props) => {
       <img className="photo-list__image" key={props.id} src={props.imageSource}></img>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.profile}></img>
-        <p className="photo-list__user-info">{props.username}</p>
-        <p className="photo-list__user-location">{props.location.city}, {props.location.country}</p>
+        <div className="photo-list__user-info">
+          <p>{props.username}</p>
+          <p className="photo-list__user-location">{props.location.city}, {props.location.country}</p>
+        </div>
       </div>
     </div>
   );
