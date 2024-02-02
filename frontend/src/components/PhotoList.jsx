ffff4c17047ac0112ff07de99b28772735234a59
DustinFader <dustinfader16@gmail.com>
2024-02-01 23:47:14 -0400

    refactor to use PhotoList

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -56,10 +57,14 @@ const sampleDataForPhotoList = [
   },
 ];
 
+const photos = new Array(sampleDataForPhotoList.length).fill(null).map((_, index) =>
+    <PhotoListItem key={sampleDataForPhotoList[index].id} {...sampleDataForPhotoList[index]}/>
+);
+
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {photos}
     </ul>
   );
 };
