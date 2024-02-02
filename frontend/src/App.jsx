16df17f643fcc112bbf68d0b309b4a8994cd3454
DustinFader <dustinfader16@gmail.com>
2024-02-02 19:39:54 -0400

    remove unneeded import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
 import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
