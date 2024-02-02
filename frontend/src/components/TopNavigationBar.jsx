f8ad158d0e208fb91b68e5f4e4dba61edb1a0be3
DustinFader <dustinfader16@gmail.com>
2024-02-02 19:32:59 -0400

    remove unneeded imports

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
-import PhotoFavButton from './PhotoFavButton';
 import FavBadge from './FavBadge';
 
 const TopNavigation = () => {
