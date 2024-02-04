1fa48b027fd1f712288f159c845dda81888bf52d
DustinFader <dustinfader16@gmail.com>
2024-02-03 20:29:48 -0400

    add details into the modal

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,10 +3,13 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
-const PhotoList = ({ photos, liked, toggleLiked, displayModal }) => {
+const PhotoList = ({ photos, liked, toggleLiked, displayModal, similar_photos }) => {
   return (
     <ul className="photo-list">
-      {photos && photos.map(photo => (
+      {Array.isArray(photos) && photos.map(photo => (
+        <PhotoListItem key={photo.id} {...photo} liked={liked} toggleLiked={toggleLiked} displayModal={displayModal}/>
+      ))}
+      {similar_photos && Object.values(similar_photos).map(photo => (
         <PhotoListItem key={photo.id} {...photo} liked={liked} toggleLiked={toggleLiked} displayModal={displayModal}/>
       ))}
     </ul>
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = (props) => {
-  
+
   useEffect(() => {
-    console.log(props.fullImage)
+    console.log(props.fullImage);
+    console.log(props.fullImage.similar_photos);
   }, [])
 
   return (
@@ -14,7 +16,18 @@ const PhotoDetailsModal = (props) => {
       <button className="photo-details-modal__close-button" onClick={() => props.displayModal(null)}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <img src={props.fullImage} />
+      <div className="photo-details-modal__images">
+        <img className='photo-details-modal__image' src={props.fullImage.urls.full} />
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={props.fullImage.user.profile}></img>
+          <div className="photo-details-modal__photographer-info">
+            <p>{props.fullImage.user.name}</p>
+            <p className="photo-details-modal__photographer-location">{props.fullImage.location.city}, {props.fullImage.location.country}</p>
+          </div>
+        </div>
+        <h4 className='photo-details-modal__header'>Similar bombastic photos</h4>
+        <PhotoList similar_photos={props.fullImage.similar_photos} liked={props.fullImage.liked} toggleLiked={props.fullImage.toggleLiked} />
+      </div>
     </div>
   )
 };
